5b15c4c10d72b761a3d113217ba545be3c4aeffd
jepoy <jefferson.claud@nuworks.ph>
2016-08-20 21:16:07 +0800

    Added updated sql

--- devcup_pee.sql
+++ devcup_pee.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 20, 2016 at 05:41 PM
+-- Generation Time: Aug 20, 2016 at 09:14 PM
 -- Server version: 5.5.49-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.17
 
@@ -29,22 +29,14 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `areas` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `image_text` text NOT NULL,
+  `image_thumb` text NOT NULL,
   `description` text NOT NULL,
   `lat` varchar(255) NOT NULL,
   `lng` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `areas`
---
-
-INSERT INTO `areas` (`id`, `image_text`, `description`, `lat`, `lng`, `created_at`, `updated_at`) VALUES
-(1, 'Images', 'Ortigas', '14.592046', '121.059396', '2016-08-20 08:48:12', '2016-08-19 23:36:53'),
-(2, 'Images', 'Megamall', '14.585103', '121.056259', '2016-08-19 23:55:37', '2016-08-19 23:55:37'),
-(3, 'Images', 'Laguna', '14.140663', '121.469177', '2016-08-19 23:55:37', '2016-08-19 23:55:37');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
