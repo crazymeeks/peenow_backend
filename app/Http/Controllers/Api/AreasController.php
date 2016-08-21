2aaa81df11ce1f0a26d3ec08788dc43d5c1c499a
jepoy <jefferson.claud@nuworks.ph>
2016-08-21 11:58:13 +0800

    Reverted the code. remove caching

--- app/Http/Controllers/Api/AreasController.php
+++ app/Http/Controllers/Api/AreasController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Contracts\AreasInterface as AreasContract;
 use App\Supports\ProcessAndValidateAreas;
 
-use phpFastCache\CacheManager;
+
 
 class AreasController extends Controller {
 	use ProcessAndValidateAreas;
@@ -18,12 +18,12 @@ class AreasController extends Controller {
 	 * @param App\Contracts\AreasInterface
 	 * @return void
 	 */
-	public function __construct(AreasContract $areas, CacheManager $cm){
+	public function __construct(AreasContract $areas){
 
-		CacheManager::setDefaultConfig(array(
-		    "path" => '/tmp', // or in windows "C:/tmp/"
-		));
-		$this->cache_instance = $cm;//CacheManager::getInstance('files');
+		// CacheManager::setDefaultConfig(array(
+		//     "path" => '/tmp', // or in windows "C:/tmp/"
+		// ));
+		// $this->cache_instance = $cm;//CacheManager::getInstance('files');
 
 		$this->areas = $areas;
 	}
--- app/Supports/ProcessAndValidateAreas.php
+++ app/Supports/ProcessAndValidateAreas.php
@@ -11,12 +11,12 @@ use Exception;
 use DB;
 use App\Areas;
 
-use phpFastCache\CacheManager;
+
 trait ProcessAndValidateAreas{
 
 	protected $areas;
 
-	protected $cache_instance;
+	
 
 	/**
 	 * Display a listing of the resource.
@@ -26,7 +26,7 @@ trait ProcessAndValidateAreas{
 	public function index()
 	{
 
-		$cache_keyword = "*";
+		/*$cache_keyword = "*";
 		$cache_instance = $this->cache_instance->getInstance('files');
     	$cache = $cache_instance->getItem($cache_keyword);
 
@@ -38,15 +38,14 @@ trait ProcessAndValidateAreas{
     		$cache->set($areas)->expiresAfter(60);//in seconds, also accepts Datetime
 	    	$cache_instance->save($cache); // Save the cache item just like you do with doctrine and 
 	    return $cache->get();
-	     }//else{
-	    // 	return $cache->get();
-	    	// $areas = DB::table('areas')
-      //                ->select(DB::raw('id, description, image_thumb, lat, lng'
-    		// 						))
-      //                ->get();	
-	    //}
-		return $cache->get();
-        //return $areas;
+	     }*/
+	    
+	    	$areas = DB::table('areas')
+                     ->select(DB::raw('id, description, image_thumb, lat, lng'
+    								))
+                     ->get();
+		//return $cache->get();
+        return $areas;
 	}
 
 	/**
@@ -86,64 +85,64 @@ trait ProcessAndValidateAreas{
 		$latlng = explode(",", $latlng);
 
 
-		$cache_keyword = count($latlng) == 1 ? 'primary_id' : 'latlng';
+		// $cache_keyword = count($latlng) == 1 ? 'primary_id' : 'latlng';
 
-		$cache_instance = $this->cache_instance->getInstance('files');
-    	$cache = $cache_instance->getItem($cache_keyword);
+		// $cache_instance = $this->cache_instance->getInstance('files');
+  //   	$cache = $cache_instance->getItem($cache_keyword);
 
-    	if($cache_keyword == 'primary_id'){
-    		if(is_null($cache->get())) {
-		    	// Get data using id(primary key)
-				if(count((array)$this->areas->find($latlng[0])) != 0){
-					$areas = $this->areas->find($latlng[0]);
-					$cache->set($areas)->expiresAfter(60);
-					$cache_instance->save($cache);
-					return $cache->get();
-					//return $this->areas->find($latlng[0]);
-				}
-				return $this->responseRequestStatus(true, "No record");
-				//return $this->responseRequestStatus(false);
-			}
-			return $cache->get();
-			//return $this->areas->find($latlng[0]);
-    	}else{
-    		$lat = $latlng[0];
-			$lng = $latlng[1];
-    		if(is_null($cache->get())){
-				//m = km x 1,000
-				$results = $this->areas->getLocationRadius($lat, $lng, true);
-				if(count((array)$results) != 0){
-					$res = array();
-					foreach($results as $result){
-						$distance_in_meter = $result->distance * 1000;
-						$res[] = array(
-							array('id' =>$result->id,
-								  'image_thumb' => $result->image_thumb,
-								  'description' => $result->description,
-								  'lat' => $result->lat,
-								  'lng' => $result->lng,
-								  'distance' => round($distance_in_meter, 2),
-							),
-						);
+  //   	if($cache_keyword == 'primary_id'){
+  //   		if(is_null($cache->get())) {
+		//     	// Get data using id(primary key)
+		// 		if(count((array)$this->areas->find($latlng[0])) != 0){
+		// 			$areas = $this->areas->find($latlng[0]);
+		// 			$cache->set($areas)->expiresAfter(60);
+		// 			$cache_instance->save($cache);
+		// 			return $cache->get();
+		// 			//return $this->areas->find($latlng[0]);
+		// 		}
+		// 		return $this->responseRequestStatus(true, "No record");
+		// 		//return $this->responseRequestStatus(false);
+		// 	}
+		// 	return $cache->get();
+		// 	//return $this->areas->find($latlng[0]);
+  //   	}else{
+  //   		$lat = $latlng[0];
+		// 	$lng = $latlng[1];
+  //   		if(is_null($cache->get())){
+		// 		//m = km x 1,000
+		// 		$results = $this->areas->getLocationRadius($lat, $lng, true);
+		// 		if(count((array)$results) != 0){
+		// 			$res = array();
+		// 			foreach($results as $result){
+		// 				$distance_in_meter = $result->distance * 1000;
+		// 				$res[] = array(
+		// 					array('id' =>$result->id,
+		// 						  'image_thumb' => $result->image_thumb,
+		// 						  'description' => $result->description,
+		// 						  'lat' => $result->lat,
+		// 						  'lng' => $result->lng,
+		// 						  'distance' => round($distance_in_meter, 2),
+		// 					),
+		// 				);
 					
-					}
-					$cache->set($res)->expiresAfter(60);
-					$cache_instance->save($cache);
-					return $cache->get();
-					//return $res;
-				}
+		// 			}
+		// 			$cache->set($res)->expiresAfter(60);
+		// 			$cache_instance->save($cache);
+		// 			return $cache->get();
+		// 			//return $res;
+		// 		}
 
-				// no record
-				return $this->responseRequestStatus(false);
-    		}
-    		return $cache->get();
-    	}
+		// 		// no record
+		// 		return $this->responseRequestStatus(false);
+  //   		}
+  //   		return $cache->get();
+  //   	}
 
 
 
 		// original code
 		//$latlng = explode(",", $latlng);
-		/*try{
+		try{
 			// Get data using id(primary key)
 			if(count($latlng) == 1){
 				if(count((array)$this->areas->find($latlng[0])) != 0){
@@ -175,7 +174,7 @@ trait ProcessAndValidateAreas{
 			return $this->responseRequestStatus(false);
 		}catch(Exception $e){
 			return $this->responseRequestStatus(false);
-		}*/
+		}
 	}
 
 }
